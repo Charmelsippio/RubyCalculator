0dcd3d1b9b2382f7a9248188093c89e4bc84ec22
Charmel Sippio <charmelsippio@gmail.com>
2016-06-21 20:47:57 -0400

    Ruby calculator homework

---
+++ calculatorHW.rb
@@ -0,0 +1,32 @@
+# Calulator Homework
+
+class Calculator
+	attr_accessor :addition, :subtraction, :multiply, :divide
+
+		def initialize(value1, value2)
+			@value1 = value1
+			@value2 = value2
+		end
+		
+		def addition()
+			@value1 + @value2
+		end
+		
+		def subtraction()
+			@value1 - @value2
+		end
+		
+		def multiply()
+			@value1 * @value2
+		end
+		
+		def divide()
+			@value1 / @value2.to_f
+		end
+end
+
+mathFunctions = Calculator.new(13, 3)
+puts mathFunctions.addition()
+puts mathFunctions.subtraction()
+puts mathFunctions.multiply()
+puts mathFunctions.divide()
